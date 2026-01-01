d9327d6a21a66e53f288f739a9edcc69ec5acf69
Innox1040 <uemj2003@gmail.com>
2026-01-01 15:50:11 +0530

    update vdo's

---
+++ vdo 54/script.js
@@ -0,0 +1,19 @@
+alert("InnoX World");
+
+console.log("Code is running");
+console.log("Code is also running");
+console.log("Code is loking like a wow");
+
+var a = prompt("Enter your name");
+var isTrue = confirm(
+  "Are you sure you want to leave this page and burn your computer "
+);
+if (isTrue) {
+  console.log("Computer is blusting");
+} else {
+  console.log("Computer is not blusting");
+}
+console.log(a);
+document.title = "lulu app";
+
+document.body.style.backgroundColor = "red";
