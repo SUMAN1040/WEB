98aeeb23698efc47af1cb6322ff24f3267ce7095
Innox1040 <uemj2003@gmail.com>
2026-01-02 14:51:40 +0530

    update vdo 55

---
+++ vdo 55/quiz.js
@@ -0,0 +1,6 @@
+let store_data = {
+  Name: "Suman Kumar Dey",
+  "Phone Number": 1234567890,
+  Marks: 100,
+};
+console.log(store_data);
---
+++ vdo 55/script.js
@@ -0,0 +1,44 @@
+console.log("Hey this is  tutorial 55 of javascript");
+
+let a = 5;
+a = a + 5;
+let b = 6;
+let c = "Suman";
+let _s = "Kumar";
+// let 55a = "Dey" //Invalid
+// console.log(a + b + 7);
+// console.log(typeof a, typeof b, typeof c);
+
+{
+  let a = 12;
+  console.log(a + b);
+}
+
+// const a1 = 45;
+// a1 = a1+44;
+// console.log(a1);
+
+let x = "Suman Kumar Dey";
+let y = 22;
+let z = 3.44;
+const p = true;
+let q = undefined;
+let r = null;
+
+console.log(x, y, z, p, q, r);
+console.log(typeof x, typeof y, typeof z, typeof p, typeof q, typeof r);
+
+// https://stackoverflow.com/questions/18808226/why-is-typeof-null-object
+
+let o = {
+  name: "Suman",
+  Age: 22,
+  isactive: true,
+};
+console.log(o);
+// console.log(typeof o);
+//for add other value
+o.city = "Bankura";
+console.log(o);
+o.city = "Jaipur";
+console.log(o);
