49e7e2cd73df3f7d11509d53a8f1fcf1bb69aa02
Innox1040 <uemj2003@gmail.com>
2026-01-03 14:58:46 +0530

    update vdo 56

---
+++ vdo 56/script.js
@@ -0,0 +1,22 @@
+console.log("Hello! I am conditional tutorial");
+
+let age = 0;
+let grace = 2;
+//if else-if else ladder
+if (age - grace > 18) {
+  console.log("You can drive as your age is: " + (age - grace));
+} else if (age == 0) {
+  console.log("Are you kidding?");
+} else {
+  console.log("You cannot drive");
+}
+
+/*
+this is a multi line comment
+*/
+
+let c = 23;
+let d = 34534;
+
+let sol = c > d ? "Good Guys" : "Bad Guys";
+console.log(sol);
