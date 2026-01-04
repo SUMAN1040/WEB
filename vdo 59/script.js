54df779ae3c8bd415c7635f9c03942bb5ee36cf6
Innox1040 <uemj2003@gmail.com>
2026-01-04 16:07:37 +0530

    update vdo 59

---
+++ vdo 59/script.js
@@ -0,0 +1,14 @@
+/*
+Create a faulty calculator using javascript
+The faulty calculator does following:
+1. Its takes two numbers as input from the user
+2. Its performs wrong operation as follows:
+
++ ---> -
+* ---> +
+- ---> /
+/ ---> *
+
+It perform wromg operation 10% of the times
+
+*/
