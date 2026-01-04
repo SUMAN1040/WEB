d2e06323a9211c51ef8e935d0197e4c439fcfdff
Innox1040 <uemj2003@gmail.com>
2026-01-04 14:26:52 +0530

    update vdo 57

---
+++ vdo 57/script.js
@@ -0,0 +1,40 @@
+console.log("I am a tutorial on loops");
+
+// let a = 1;
+// console.log(a);
+
+// for(let i = 0; i<= 10; i++){
+//     console.log(i);
+// }
+
+let obj = {
+    name: "Suman Kumar Dey",
+    age: 22,
+    company: "InnoX"
+}
+// for (const key in obj) {
+//     // if (!Object.hasOwn(obj, key)) continue;
+//     const element = obj[key];
+//     console.log(key);
+// }
+
+// for (const char of "Suman") {
+//     console.log(char);
+// }
+
+
+// let v = 0;
+// while(v<6){
+//     console.log(v);
+//     v++;
+// }
+
+
+//do while loop
+// let r = 0;
+// do{
+//     console.log(r);
+//     r++;
+// }while(r<6);
+
+
