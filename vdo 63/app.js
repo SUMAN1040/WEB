173f0000c652585e78ded3f926e4e0c78d091c17
Innox1040 <uemj2003@gmail.com>
2026-01-07 22:50:02 +0530

    update

---
+++ vdo 63/app.js
@@ -0,0 +1,10 @@
+let arr = [1, 2, 3, 4, 5];
+console.log(arr);
+console.log(arr.length);
+
+//Arrays are mutable
+arr[0] = 10;
+console.log(arr);
+console.log(typeof arr);
+console.log(arr.toString());
+console.log(arr.join(" and "));
