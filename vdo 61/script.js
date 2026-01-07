d37089f21993b7c1d92194c70886ea90adc2e9d8
Innox1040 <uemj2003@gmail.com>
2026-01-07 20:18:20 +0530

    update all

--- vdo 61/script.js
+++ vdo 61/script.js
@@ -14,10 +14,34 @@ let obj = {
 if (random > 0.1) {
   console.log(`The result is ${a} ${c} ${b}`);
   alert(`The result is ${eval(`${a} ${c} ${b}`)}`);
-} 
-
+}
 
 else {
   c = obj[c];
   alert(`The result is ${eval(`$a ${c} $b`)}`);
 }
+
+
+
+//Using another method
+
+// let ope = prompt("Enter operator(either + or - or * or /): ");
+
+// let number1 = prompt("Enter first number: ");
+// let number2 = prompt("Enter second number: ");
+
+// let r = Math.random();
+// console.log("This is random number", r);
+
+// let result;
+// if (r < 0.1 && ope == `+`) {
+//   result = number1 - number2;
+// } else if (r < 0.1 && ope == `*`) {
+//   result = number1 + number2;
+// } else if (r < 0.1 && ope == `-`) {
+//   result = number1 / number2;
+// } else if (r < 0.1 && ope == `/`) {
+//   result = number1 ** number2;
+// }
+
+// console.log(result);
---
+++ vdo 62/app.js
@@ -0,0 +1,22 @@
+//Create a bussiness name generator by combining list of activities and shop name and another word
+
+
+/*
+Adjectives:
+Crazy
+Amazing
+Fire
+
+
+Shop name:
+Engine
+Foods
+Garments
+
+
+Another Word:
+Bros
+Limited
+Hub
+*/
+
