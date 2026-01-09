4143ba686550d94adb79469b7e6d28511fada502
Innox1040 <uemj2003@gmail.com>
2026-01-09 19:10:21 +0530

    update 64 extra

--- vdo 64/script.js
+++ vdo 64/script.js
@@ -19,36 +19,59 @@ Limited
 Hub
 */
 
-let first, second, third;
+// let first, second, third;
 
-//let's generate the first word
-let rand = Math.random();
-if (rand < 0.33) {
-  first = "Creazy";
-} else if (rand < 0.66 && rand >= 0.33) {
-  first = "Amazing";
-} else {
-  first = "Fire";
-}
+// //let's generate the first word
+// let rand = Math.random();
+// if (rand < 0.33) {
+//   first = "Creazy";
+// } else if (rand < 0.66 && rand >= 0.33) {
+//   first = "Amazing";
+// } else {
+//   first = "Fire";
+// }
 
-//lets generate the second word
-rand = Math.random();
-if (rand < 0.33) {
-  second = "Engine";
-} else if (rand < 0.66 && rand >= 0.33) {
-  second = "Foods";
-} else {
-  second = "Garments";
-}
+// //lets generate the second word
+// rand = Math.random();
+// if (rand < 0.33) {
+//   second = "Engine";
+// } else if (rand < 0.66 && rand >= 0.33) {
+//   second = "Foods";
+// } else {
+//   second = "Garments";
+// }
 
-//lets generate the third word
-rand = Math.random();
-if (rand < 0.33) {
-  third = "Bros";
-} else if (rand < 0.66 && rand >= 0.33) {
-  third = "Limited";
-} else {
-  third = "Hub";
-}
+// //lets generate the third word
+// rand = Math.random();
+// if (rand < 0.33) {
+//   third = "Bros";
+// } else if (rand < 0.66 && rand >= 0.33) {
+//   third = "Limited";
+// } else {
+//   third = "Hub";
+// }
 
-console.log(`${first} ${second} ${third}`);
+// console.log(`${first} ${second} ${third}`);
+
+// other method
+let obj1 = {
+  1: "Creazy",
+  2: "Amazing",
+  3: "Fire",
+};
+let obj2 = {
+  1: "Engine",
+  2: "Foods",
+  3: "Garments",
+};
+let obj3 = {
+  1: "Bros",
+  2: "Limited",
+  3: "Hub",
+};
+
+var ran1 = Math.floor(Math.random() * 3) + 1;
+var ran2 = Math.floor(Math.random() * 3) + 1;
+var ran3 = Math.floor(Math.random() * 3) + 1;
+
+console.log(`${obj1[ran1]} ${obj2[ran2]} ${obj3[ran3]}`);
