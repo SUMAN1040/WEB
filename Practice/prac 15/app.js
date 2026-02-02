6040ca98d37c8175c8f0e9aeb0d0220035d1557d
Innox1040 <uemj2003@gmail.com>
2026-02-02 22:32:35 +0530

    update js past

--- Practice/prac 15/app.js
+++ Practice/prac 15/app.js
@@ -1,5 +1,19 @@
-function greet() {
-  console.log("Hello! Suman");
-}
+// function greet() {
+//   console.log("Hello! Suman");
+// }
 
-greet();
+// greet();
+
+
+// Converting to String
+String(123);        // "123"
+String(true);       // "true"
+String(null);       // "null"
+(123).toString();   // "123"
+
+// Converting to Number
+Number("123");    // 123
+Number("3.14");   // 3.14
+Number(true);     // 1
+Number(false);    // 0
+Number("hello");  // NaN (Not a Number)
