81c3066ce990c0c5ced21501317a47c3d60691af
Innox1040 <uemj2003@gmail.com>
2026-02-10 01:06:53 +0530

    update vdo 69

---
+++ vdo 69/app.js
@@ -0,0 +1,22 @@
+
+// function factorial(num){
+//     let arr = Array.from(Array(num + 1).keys());
+//     let c = arr.slice(1,).reduce((a, b) => {
+//         return a * b;
+//     })
+//     console.log(c);
+// }
+
+
+
+function fact(num){
+    let fact = 1;
+    for(let index = 0; index <= number; index++){
+        fact = fact * index;
+    }
+    return fact;
+}
+
+let a = 5
+console.log(factorial(a));
+console.log(fact(a));
