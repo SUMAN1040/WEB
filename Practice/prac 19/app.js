3e1446e6861d125196f94e91ad1439be2904a30b
Innox1040 <uemj2003@gmail.com>
2026-02-10 19:55:43 +0530

    update prac 19

---
+++ Practice/prac 19/app.js
@@ -0,0 +1,32 @@
+//Array and Array Methods
+var myArray = [1, "Hello", [2, 3]];
+console.log(myArray.length);
+
+console.log()
+
+myArray.push("World");
+console.log(myArray);
+
+console.log()
+
+myArray.pop();
+console.log(myArray);
+
+console.log();
+
+myArray.shift();
+console.log(myArray);
+
+console.log();
+
+myArray.unshift("Suman");
+console.log(myArray);
+
+console.log();
+
+console.log(myArray.slice(1, 2));
+
+console.log();
+
+myArray.slice(1, 1, "Hello World", [4, 5]);
+console.log(myArray);
