061adfeb1b2ad9a6423105ecafbce25e5392d8fc
Innox1040 <uemj2003@gmail.com>
2026-02-13 23:21:27 +0530

    update prac 24

---
+++ Practice/prac 24/app.js
@@ -0,0 +1,18 @@
+//Window Objects
+// document.getElementById("Suman").innerHTML = "This is my new Text";
+
+// // window.location.href = "https://www.google.com";
+
+// alert("This is an alert message");
+// confirm("Make yourself as a proper person?");
+// prompt("Enter your name");
+
+
+//History Objects
+
+// history.pushState(null, null, "https://www.google.com");
+// history.back();
+
+
+//Naviage
+
