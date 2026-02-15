7422073b3f58d7a9e626048e0a80cdbe765d84ac
Innox1040 <uemj2003@gmail.com>
2026-02-15 17:17:20 +0530

    update prac 24

--- Practice/prac 24/app.js
+++ Practice/prac 24/app.js
@@ -7,12 +7,10 @@
 // confirm("Make yourself as a proper person?");
 // prompt("Enter your name");
 
-
 //History Objects
 
 // history.pushState(null, null, "https://www.google.com");
 // history.back();
-
 
 //Naviage
 
@@ -32,19 +30,14 @@
 // console.log(`The user screen availabe width is${screen.availWidth}`);
 // console.log(`The user screen availabe height is${screen.availHeight}`);
 
-
 // console.log(navigator.geolocation.getCurrentPosition((position) => {
 //     console.log(position);
 // }));
-
-
 
 // // Screen Object
 // console.log(`The screen resolution is ${screen.width}x${screen.height} pixels`);
 // console.log(`The screen color depth is ${screen.colorDepth} bits`);
 // console.log(`The available screen width is ${screen.availWidth} pixels and the available screen height is ${screen.availHeight} pixels`)
-
-
 
 // Document Object
 // let elements = document.getElementsByClassName("container");
@@ -56,11 +49,26 @@
 // newDiv.className = "div1";
 // document.body.appendChild(newDiv);
 
-
-
 // getElementsById()
 // let myDiv = document.getElementById("Suman");
 // myDiv.innerHTML = "Good Morning";
 
-
 //getElementsByClassName()
+// let elements = document.getElementsByClassName("box");
+// for (let i = 0; i < elements.length; i++) {
+//   elements[i].innerHTML = "This is created by getElementsByClassName";
+// }
+
+//getElementsByTagName()
+// let elements = document.getElementsByTagName("p");
+// for (let i = 0; i < elements.length; i++) {
+//     elements[i].innerHTML = "This is my new text";
+// }
+
+//InnerHTML
+// let myDiv = document.getElementById("myDiv");
+// myDiv.innerHTML = "This is my new text";
+
+//outerHTML
+let myDiv = document.getElementById("myDiv");
+myDiv.outerHTML = "<div><input type='text' name='myName' value='Enter the name: '/></div>";
