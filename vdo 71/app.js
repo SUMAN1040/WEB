d23d5804eb7c5f9bdeb9b51800dd819ba51431f6
Innox1040 <uemj2003@gmail.com>
2026-02-15 20:01:09 +0530

    update cdo 71

---
+++ vdo 71/app.js
@@ -0,0 +1,8 @@
+// let div = document.createElement("div");
+// div.innerHTML = "Hello World";
+// div.setAttribute("class", "created");
+// document.querySelector(".container").prepend(div);
+
+
+let cont = document.querySelector(".container");
+cont.insertAdjacentHTML("afterbegin", "<div>Hello, I am Suman kumar Dey</div>");
