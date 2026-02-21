5fd96b2900e0f09b239a1adbd951de9a0aa3dd80
Innox1040 <uemj2003@gmail.com>
2026-02-21 22:40:17 +0530

    add vdo 75

---
+++ vdo 75/script.js
@@ -0,0 +1,8 @@
+console.log("Suman is a student");
+console.log("Suman is an Engineering Student");
+
+setTimeout(() => {
+    console.log("Suman is a student");
+}, 5000);
+
+console.log("The End");
